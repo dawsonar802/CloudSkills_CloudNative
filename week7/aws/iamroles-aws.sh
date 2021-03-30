42a4c92fd62af6b63f4e0920cd50937cbccb42d1
Andrew <dawson_a17@yahoo.com>
2021-03-30 19:47:39 -0400

    week7 commit

---
+++ week7/aws/iamroles-aws.sh
@@ -0,0 +1,5 @@
+aws iam create-user --user-name A2D2
+
+aws iam create-group --group-name Andrews-Group
+
+aws iam add-user-to-group --user-name A2D2 --group-name Andrews-Group
---
+++ week7/aws/rbac-rules-azure.sh
@@ -0,0 +1,3 @@
+az ad sp create-for-rbac -n "AzureDevOps" --role Contributor --scopes subscriptions/f8b3739e-6523-45e1-bf08-bf628206474e
+
+az ad sp create-for-rbac -n "AzureDevOps" --role Contributor --scopes subscriptions/f8b3739e-6523-45e1-bf08-bf628206474e --sdk-auth
